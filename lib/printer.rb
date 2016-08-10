b50df3da082a195f895dca7dd4c1737846f26278
toby676 <tjcmui@gmail.com>
2016-08-10 14:30:21 +0100

    refactor

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,24 +2,23 @@ class Printer
 
   def initialize(account_history)
     @account_history = account_history
-    @string = "date || credit || debit || balance\n"
+    @string = ""
   end
 
   def print_statement
-    format_statement
-    @string
+    prepare_statement
+    "date || credit || debit || balance\n" + @string
   end
 
   private
-  def format_statement
-    @account_history.reverse.collect do |transaction|
-    @string << "#{transaction.date.strftime('%d/%m/%Y')} || "
-      if transaction.amount < 0
-        @string << "|| #{'%.2f' % -transaction.amount} || "
-      else
-        @string << "#{'%.2f' % transaction.amount} || || "
-      end
-    @string << "#{'%.2f' % transaction.balance}\n"
+  def format_line(transaction)
+    "#{transaction.date.strftime('%d/%m/%Y')} || " +
+    (transaction.amount < 0 ? "|| #{'%.2f' % -transaction.amount} || " : "#{'%.2f' % transaction.amount} || || ") +
+    "#{'%.2f' % transaction.balance}\n"
+  end
+  def prepare_statement
+    @account_history.reverse.each do |transaction|
+      @string << format_line(transaction)
     end
   end
 
